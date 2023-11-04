1ca3beae5a333bdabd5bbdab0557aa71c2334407
hailhartz <122579025+hailhartz@users.noreply.github.com>
2023-11-04 14:39:57 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,65 @@
+
+
+addEventListener("load", function() {
+    let canvas = this.document.querySelector("canvas");
+    console.log(canvas);
+    let ctx = canvas.getContext("2d");
+
+
+    this.document.getElementById("draw_hazel").addEventListener("click", function() {
+        let img = document.querySelector("img#hazel");
+        ctx.drawImage(img, 0, 0, 600, 600);
+        ctx.font = "60px Arial";
+        ctx.fillStyle = "red";
+        ctx.fillText("Hazel", 200, 50);
+        
+    });
+
+    this.document.getElementById("draw_biscuit").addEventListener("click", function() {
+        let img = document.querySelector("img#biscuit");
+        ctx.drawImage(img, 0, 0, 800, 600);
+        ctx.font = "60px Arial";
+        ctx.fillStyle = "purple";
+        ctx.fillText("Biscuit Dog", 150, 50);
+    });
+
+    this.document.getElementById("draw_bacon").addEventListener("click", function() {
+        let img = document.querySelector("img#bacon");
+        ctx.drawImage(img, 0, 0, 600, 600);
+        ctx.font = "60px Arial";
+        ctx.fillStyle = "red";
+        ctx.fillText("Bacon", 200, 50);
+    });
+
+    this.document.getElementById("draw_bear").addEventListener("click", function() {
+        let img = document.querySelector("img#bear");
+        ctx.drawImage(img, 0, 0, 600, 800);
+        ctx.font = "60px Arial";
+        ctx.fillStyle = "red";
+        ctx.fillText("Bear", 200, 50);
+    });
+
+    this.document.getElementById("draw_millie").addEventListener("click", function() {
+        let img = document.querySelector("img#millie");
+        ctx.drawImage(img, 0, 0, 600, 800);
+        ctx.font = "60px Arial";
+        ctx.fillStyle = "red";
+        ctx.fillText("Millie", 200, 50);
+    });
+
+    this.document.getElementById("draw_molly").addEventListener("click", function() {
+        let img = document.querySelector("img#molly");
+        ctx.drawImage(img, 0, 0, 600, 600);
+        ctx.font = "60px Arial";
+        ctx.fillStyle = "purple";
+        ctx.fillText("Molly", 200, 50);
+    });
+
+    this.document.getElementById("draw_carter").addEventListener("click", function() {
+        let img = document.querySelector("img#carter");
+        ctx.drawImage(img, 0, 0, 600, 600);
+        ctx.font = "60px Arial";
+        ctx.fillStyle = "red";
+        ctx.fillText("Carter", 200, 50);
+    });
+});
